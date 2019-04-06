8da437dfbbeffc043cc57671185d365582558e4c
ksteensig <kasper.steensig@gmail.com>
2019-04-06 13:44:31 +0200

    Add new pages

--- main.py
+++ main.py
@@ -10,22 +10,31 @@ def create_label(text):
 c_cert_assistant = Gtk.Assistant()
 c_info_grid = Gtk.Grid()
 c_info_grid.set_row_spacing(6)
+c_info_grid.set_column_spacing(20)
 
 c_your_name = create_label("Your name")
 c_your_mail = create_label("Your email")
 c_their_name = create_label("Their name")
 c_their_mail = create_label("Their email")
 
-#c_enter_your_name = create_entry("", True)
-#c_enter_your_mail = create_entry("", True)
-#c_enter_their_name = create_entry("", True)
-#c_enter_their_mail = create_entry("", True)
+c_enter_your_name = Gtk.Entry()
+c_enter_your_mail = Gtk.Entry()
+c_enter_their_name = Gtk.Entry()
+c_enter_their_mail = Gtk.Entry()
 
 c_cert_assistant.append_page(c_info_grid)
+
 c_info_grid.attach(c_your_name, 0, 0, 1, 1)
 c_info_grid.attach(c_your_mail, 0, 1, 1, 1)
 c_info_grid.attach(c_their_name, 0, 2, 1, 1)
 c_info_grid.attach(c_their_mail, 0, 3, 1, 1)
+
+c_info_grid.attach(c_enter_your_name, 1, 0, 1, 1)
+c_info_grid.attach(c_enter_your_mail, 1, 1, 1, 1)
+c_info_grid.attach(c_enter_their_name, 1, 2, 1, 1)
+c_info_grid.attach(c_enter_their_mail, 1, 3, 1, 1)
+
+c_cert_assistant.append_page(c_info_grid)
 
 #mail_window = Gtk.Window()
 
