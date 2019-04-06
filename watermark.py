b75390349fd6304df7aa8f4103c7f436fb32ee07
Luraman <ponifi3d@gmail.com>
2019-04-06 21:22:41 +0200

    Added black option for watermark

--- watermark.py
+++ watermark.py
@@ -15,35 +15,43 @@ class Watermark:
     def __init__(self, sender, smail, recipient, rmail):
         self.internal = (sender, smail, recipient, rmail)
 
-    def __call__(self, image):
+    def __call__(self, image, model, negate=False):
         self.font.push()
         size = max(image.width, image.height)
 
         result = image.clone()
-        sendertext = " Sender: {0} \n Sender-mail: {1} ".format(*self.internal)
-        receivertext = " Recipient: {2} \n Recipient-mail: {3} ".format(*self.internal)
+        sendertext = " Sender: {0} \n Recipient: {2} ".format(*self.internal)
+        receivertext = " Sender-mail: {1} \n Recipient-mail: {3} ".format(*self.internal)
         #wraptext = wrap(text, 20)
         fontsize = size//40
         self.font.font_size = fontsize
         textheight = 3*image.height//5
         sendwidth = int(self.font.get_font_metrics(image, sendertext, True).text_width)
+        receivewidth = int(self.font.get_font_metrics(image, receivertext, True).text_width)
 
+        if negate:
+            self.font.fill_color = Color("Black")
+            self.font.fill_opacity = 0.5
         self.font.text(fontsize, textheight, sendertext)
         self.font.text(fontsize + sendwidth, textheight, receivertext)
+        self.font.font_size = int(1.5*fontsize)
+        self.font.text(fontsize, textheight + fontsize * 3, " Model: {0}".format(model))
         self.font.draw(result)
 
         with self.stud.clone() as newstud:
             if size < 4000:
                 newstud.resize(size, size)
+            if negate:
+                newstud.negate()
             result.composite(newstud, 0, textheight - (newstud.height + fontsize))
-            result.composite(newstud, 0, textheight + fontsize)
+            result.composite(newstud, 0, textheight + fontsize * 2)
 
         self.font.pop()
         return result
 
 
 with Image(filename='Klodsmajor.JPG') as img:
-    marking = Watermark("Me", "gmail", "You", "hotmail")
-    result = marking(img)
+    marking = Watermark("Thomas Frandsen", "thomas.frandsen@hotmail.com", "Kasper Steensig", "kasper.steensig@gmail.com")
+    result = marking(img, "27803", True)
     result.save(filename='Edit.png')
     display(result)
